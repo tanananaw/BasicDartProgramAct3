db3397f73cf2aa0acfbd6c2261aac42cbf001d49
Barcebal <168712262+tanananaw@users.noreply.github.com>
2025-08-08 08:47:27 +0000

    Dart simple calculator program.

---
+++ Calculator.dart
@@ -0,0 +1,14 @@
+void main() {
+  num a = 20;
+  num b = 3;
+
+  print('Fixed Numbers Calculator');
+  print('First number: $a');
+  print('Second number: $b\n');
+
+  print('Addition: ${a + b}');
+  print('Subtraction: ${a - b}');
+  print('Multiplication: ${a * b}');
+  print('Division: ${a / b}');
+  print('Modulo: ${a % b}');
+}
